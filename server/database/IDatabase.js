7542a7d2c67da1151636bf217da48705c3b68340
ismail <ismailmagdy88@gmail.com>
2023-08-21 18:55:00 +0300

    added Database

---
+++ server/database/IDatabase.js
@@ -0,0 +1,3 @@
+class IDatabase{
+    async connect();
+}
---
+++ server/database/MongoDB.js
@@ -0,0 +1,30 @@
+const mongoose = require("mongoose");
+const env = require('dotenv');
+env.config({path:'../config.env'});
+
+
+let instance = null;
+class MongoDB extends IDatabase{
+    #connectionString;
+    constructor(connectionString) {
+        super();
+        this.#connectionString = connectionString;
+    }
+    async connect() {
+        await mongoose.connect(this.#connectionString, {
+            useNewUrlParser: true,
+            useCreateIndex: true,
+            useUnifiedTopology: true,
+            useFindAndModify: false,
+        });
+        console.log("Successful DB Connection");
+    }
+}
+
+
+module.exports = function getInstance(){
+    if(instance === null){
+        instance = new MongoDB(process.env.CONNECTION_STRING);
+    }
+    return instance;
+}
