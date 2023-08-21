742a84682b4bfb37d937af92aa0f6c748d64d862
ismail <ismailmagdy88@gmail.com>
2023-08-21 19:41:53 +0300

    Tested Running Server

--- server/server.js
+++ server/server.js
@@ -1,12 +1,16 @@
 const env = require('dotenv');
-const PORT = process.env.PORT;
+const app = require('./app');
 const mongoDB = require('./database/MongoDB');
 
+const PORT = process.env.PORT;
 env.config({path:'./config.env'});
 
 
 const runServer = async function (){
     await  mongoDB.getInstance().connect();
+    app.listen(()=>{
+        console.log(`Server Listening at PORT ${PORT}....`);
+    })
 }
 
 runServer();
