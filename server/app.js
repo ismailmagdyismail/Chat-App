916144c2e503d6ac3417996d19f55ce30aadc837
ismail <ismailmagdy88@gmail.com>
2023-08-23 01:56:33 +0300

    Added Middlre-Ware for sanitization , logging , parsing , ratelimitng , prevention(sqlInj,XSS,pollution)

--- server/app.js
+++ server/app.js
@@ -1,6 +1,28 @@
 const express = require('express');
+const hpp = require('hpp');
+const cookieParser = require('cookie-parser');
+const xssClean = require('xss-clean');
+const helmet = require('helmet');
+const mongoSanitize = require('express-mongo-sanitize');
+const morgan  = require('morgan');
+const rateLimiter = require('express-rate-limit');
 
 const  app = express();
+app.use(morgan('dev'));       /// Logging
+app.use(helmet());           /// Add-Protection-Headers
+app.use(express.json());    /// Body-Parsing
+app.use(cookieParser());   /// HTML-Cookie-Parsing
+app.use(mongoSanitize()); /// Prevent-SQL-Injection
+app.use(hpp());          /// Prevent-Parameter-Pollution
+app.use(xssClean());    /// Prevent Cross-site-forgery
+
+const limiter = rateLimiter({
+    max:100,
+    windowMs:60*60*1000,
+    message:"To many requests from same IP , Try again later",
+});
+app.use('/',limiter); /// Rate-Limiting
+
 
 
 
