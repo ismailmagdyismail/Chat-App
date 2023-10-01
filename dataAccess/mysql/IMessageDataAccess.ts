e9ba05ba48274c8217242021b061731fddd0d68d
Ismail Magdy <92678464+ismailmagdyismail@users.noreply.github.com>
2023-10-01 22:59:20 +0300

    Delete dataAccess/mysql/IMessageDataAccess.ts

--- dataAccess/mysql/IMessageDataAccess.ts
+++
@@ -1,8 +0,0 @@
-import {Message} from "../models/messageModel";
-
-export interface IMessageDataAccess{
-    getMessageById(id:string):Promise<Message|null>
-    getMessagesByConversation(conversationId:string):Promise<Message[]|null>
-    createMessage(message:Message):Promise<Message|null>
-    deleteMessage(id:string):Promise<void>
-}
