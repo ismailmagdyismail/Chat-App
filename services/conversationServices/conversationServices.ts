9c62dff4d3e5eb6554a6902a5098b753d70cc6d5
Ismail Magdy <92678464+ismailmagdyismail@users.noreply.github.com>
2023-10-01 22:45:27 +0300

    Add files via upload

--- services/conversationServices/conversationServices.ts
+++ services/conversationServices/conversationServices.ts
@@ -1,11 +1,16 @@
 import {Conversation} from "../../models/conversationModel";
 import server from "../../server";
 import {User} from "../../models/userModel";
-import {getUserByPhoneService} from "../userServices/userServices";
+import {getUserByIdService, getUserByPhoneNumberService} from "../userServices/userServices";
+import {AppError} from "../../errors/AppError";
 
 
 export async function getUserConversationsService(userId:string):Promise<Conversation[]|null>{
     try {
+        const user:User|null = await getUserByIdService(userId);
+        if(!user){
+            throw new AppError(404,`USER with ID ${userId} doesn't Exist`);
+        }
         return await server.getDataAccessLayer().getUserConversations(userId);
     } catch (err) {
         throw err;
@@ -18,18 +23,21 @@ export async function getConversationService(conversationId:string):Promise<Conv
         throw err;
     }
 }
-export async function createConversationService(firstMemberPhoneNumber: string, secondMemberPhoneNumber: string): Promise<Conversation | null> {
+export async function createConversationService(firstMemberPhoneNumber: string, secondMemberPhoneNumber: string): Promise<Conversation> {
     try {
-        const firstUser: User | null = await getUserByPhoneService(firstMemberPhoneNumber);
-        const secondUser: User | null = await getUserByPhoneService(secondMemberPhoneNumber);
+        const [firstUser,secondUser]:[User|null,User|null] = await Promise.all([getUserByPhoneNumberService(firstMemberPhoneNumber),getUserByPhoneNumberService(secondMemberPhoneNumber)]);
         if (!firstUser || !secondUser) {
-            throw new Error(`Phone Number ${!firstUser && firstMemberPhoneNumber} ${!secondUser && secondMemberPhoneNumber} doesn't exist`);
+            throw new AppError(404,`Phone Number ${!firstUser && firstMemberPhoneNumber} ${!secondUser && secondMemberPhoneNumber} doesn't exist`);
         }
         const conversation: Conversation = {
             firstMemberId: firstUser.id!,
             secondMemberId: secondUser.id!
         }
-        return await server.getDataAccessLayer().createConversation(conversation);
+        const newConversation = await server.getDataAccessLayer().createConversation(conversation);
+        if(!newConversation){
+            throw new AppError(500,"Error Creating new Conversation");
+        }
+        return newConversation
     } catch (err) {
         throw err;
     }
