6b771e15b4af44834ff2a3508090f9ed8d2a995f
Ismail Magdy <92678464+ismailmagdyismail@users.noreply.github.com>
2023-10-13 22:47:31 +0300

    Delete dataAccess directory

--- dataAccess/IConversationDataAccess.ts
+++
@@ -1,8 +0,0 @@
-import {Conversation} from "../models/conversationModel";
-
-export interface IConversationDataAccess{
-    getConversationById(id:string):Promise<Conversation|null>
-    createConversation(conversation:Conversation):Promise<Conversation|null>
-    getUserConversations(id:string):Promise<Conversation[]|null>
-    deleteConversation(id:string):Promise<void>
-}
--- dataAccess/IUserDataAccess.ts
+++
@@ -1,7 +0,0 @@
-import {User} from "../models/userModel";
-
-export interface IUserDataAccess{
-    getUserById(id:string):Promise<User|null>;
-    getUserByPhoneNumber(phoneNumber:string):Promise<User|null>;
-    createUser(user:User):Promise<User|null>;
-}
