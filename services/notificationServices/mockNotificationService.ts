aa273e6d76b760e15ddb1858643e7834e820f8a8
Ismail Magdy <92678464+ismailmagdyismail@users.noreply.github.com>
2023-10-13 22:52:49 +0300

    Delete services/notificationServices directory

--- services/notificationServices/mockNotificationService.ts
+++
@@ -1,6 +0,0 @@
-import {INotification} from "./INotification";
-
-export class MockNotificationService implements INotification{
-    sendNotification(from: string, to: string, content: string):void {
-    }
-}
