91a57220580093a21d3fcbab3946d546afca59c9
Ismail Magdy <92678464+ismailmagdyismail@users.noreply.github.com>
2023-10-21 03:24:07 +0300

    Add files via upload

--- server/app.ts
+++ server/app.ts
@@ -28,31 +28,6 @@ app.use('/api/v1/conversations/',conversationRouter);
 app.use('/api/v1/messages/',messageRouter);
 
 
-app.get('/messages/:id',async function (req:Request,res:Response){
-    const message:Message|null = await server.getDataAccessLayer().getMessageById(req.params.id);
-    res.status(200).json({
-        data:message
-    });
-});
-app.get('/conversations/:conversationId/messages/',async function (req:Request,res:Response){
-    const messages:Message[]|null = await server.getDataAccessLayer().getMessagesByConversation(req.params.conversationId);
-    res.status(200).json({
-        data:messages
-    });
-});
-app.post('/conversations/:id',async function (req:Request,res:Response){
-    const message:Message ={
-        content:req.body.content,
-        sentBy:req.body.send_by,
-        conversationId:req.body.conversation_id,
-    };
-    const newMessage:Message|null = await server.getDataAccessLayer().createMessage(message);
-    res.status(201).json({
-       data:newMessage,
-    });
-});
-
-// app.use(logger)
 app.use((req:Request,res:Response,next:NextFunction):void=>{
     next(new AppError(404,"Route not defined"));
 })
